8886994e8ceda745f3a70d5ecc1261988f62eb07
jcwlim <jforum@me.com>
2023-07-21 22:29:45 +0800

    Adjust car position

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -21,7 +21,7 @@ TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 # model path is in the frame of the camera
 if EON:
-  CAMERA_OFFSET = 0.25
+  CAMERA_OFFSET = 0.20
   PATH_OFFSET = 0.0
 else:
   CAMERA_OFFSET = 0.04
