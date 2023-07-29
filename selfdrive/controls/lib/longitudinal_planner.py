3babce50a23fd24e49ac4a2d878f77e740d05570
jcwlim <jforum@me.com>
2023-07-29 10:40:34 +0800

    Adjust standard for profile adjustment

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -246,9 +246,10 @@ class LongitudinalPlanner:
         y_dist = [1.0,  1.1,  1.2,    1.2,    1.34,   1.34,   1.34]
         desired_tf = np.interp(v_ego, x_vel, y_dist)
       elif self.dp_following_profile == 1:
-        x_vel =  [5.556, 19.7,   41.67]
+        #x_vel =  [5.556, 19.7,   41.67]
+        x_vel =  [0, 5.556, 19.7,   41.67]
         #y_dist = [1.4,   1.6,    1.6 ]
-        y_dist = [1.2,   1.0,    1.0 ]
+        y_dist = [1.4, 1.1,   1.0,    1.0 ]
         desired_tf = np.interp(v_ego, x_vel, y_dist)
       elif self.dp_following_profile == 2:
         x_vel =  [0,     19.7,   41.67]
