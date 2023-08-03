b04fca69a31ea8735e81568050d219c4ce71df6b
jcwlim <jforum@me.com>
2023-08-03 17:25:49 +0800

    Adjust vEgoStopping to 0.2

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -246,7 +246,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True
     ret.vEgoStarting = 0.1
-    ret.vEgoStopping = 0.1
+    ret.vEgoStopping = 0.2
     ret.startAccel = 1.0
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
