9db5a54a6daff449ac7252c41b380434447773ba
jasonlim <jforum@me.com>
2024-03-06 19:26:17 +0800

    Try to make it smoother for stop and start

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -290,8 +290,8 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True
     ret.vEgoStarting = 0.4
-    ret.vEgoStopping = 0.1
-    ret.startAccel = 1.0
+    ret.vEgoStopping = 0.06 #0.1
+    ret.startAccel = 0.1 #1.0
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
