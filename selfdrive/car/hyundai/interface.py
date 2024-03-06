e56a625afdeef473ea841c7eff9a5ac9c1283e21
jcwlim <jforum@me.com>
2024-03-06 23:10:16 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -290,7 +290,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True
     ret.vEgoStarting = 0.4
-    ret.vEgoStopping = 0.06 #0.1
+    ret.vEgoStopping = 0.08 #0.1
     ret.startAccel = 0.1 #1.0
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
